19f112f78bf9ae24f22620460b4387b72c1f69c4
yedol1 <57481378+yedol1@users.noreply.github.com>
2019-11-19 19:16:35 +0900

    ming

---
+++ MenuFrameViewer.java
@@ -0,0 +1,15 @@
+import javax.swing.JFrame;
+
+/**
+   This program tests the MenuFrame.
+*/
+public class MenuFrameViewer
+{
+   public static void main(String[] args)
+   {
+      JFrame frame = new MenuFrame();
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      frame.setVisible(true);
+   }
+}
+
