a13ba9a231329e2d667814c12376ae5315a172c7
youjeonghan <57481424+youjeonghan@users.noreply.github.com>
2019-12-03 00:05:53 +0900

    12/3 12:05

--- 자바 팀플/src/자바팀플/Fish.java
+++ 자바 팀플/src/자바팀플/Fish.java
@@ -47,10 +47,22 @@ public class Fish implements ActionListener, KeyListener {
 	public static Timer timerheart;
 	public static Timer timerfish0;
 	public static Timer timerfishbone;
-	public static int speed;
+	public static int speed=1;
 	public static int penguinwidth = 80;
 	public static int penguinheight = 90;
 	public static int cnt=0;
+	public static int sx=0;
+	public static int sy=0;
+	public boolean gameOverSound = false;
+	public static ImageIcon i1 = new ImageIcon("image/back1.jpg");		//배경 이미지
+	public static ImageIcon iheart1 = new ImageIcon("image/heart1.png");//현재 라이프 이미지삽입		
+	public static ImageIcon penguin1= new ImageIcon("image/penguin1.png");//펭귄이미지
+	public static ImageIcon penguin2= new ImageIcon("image/penguin2.png");
+	public static ImageIcon penguin3= new ImageIcon("image/penguin3.png");
+	public static ImageIcon ifish = new ImageIcon("image/fish.png");
+	public static ImageIcon iheart0 = new ImageIcon("image/heart0.png");
+	public static ImageIcon ifishbone = new ImageIcon("image/fishbone.png");
+	public static int fishbonedelay = 1600;
 	public Fish() {
 		JFrame jframe = new JFrame();
 		timerfish0 = new Timer(1500, new ActionListener() { // 생선 객체 생성
@@ -65,13 +77,13 @@ public class Fish implements ActionListener, KeyListener {
 				heart0.add(new Rectangle(dropheart + 40, 0, 40, 40));
 			}
 		});
-		timerfishbone = new Timer(900, new ActionListener() { // 생선뼈 객체 생성
+		timerfishbone = new Timer(fishbonedelay, new ActionListener() { // 생선뼈 객체 생성
 			public void actionPerformed(ActionEvent arg0) {
 				int dropfishbone = rand.nextInt(WIDTH - 80);
 				fishbone0.add(new Rectangle(dropfishbone + 40, 0, 40, 40));
 			}
 		});
-		Timer timer2 = new Timer(3, this);
+		Timer timer2 = new Timer(10, this);
 		panel = new Mypanel();
 		rand = new Random();
 		jframe.add(panel);
@@ -89,15 +101,14 @@ public class Fish implements ActionListener, KeyListener {
 		heart0 = new ArrayList<Rectangle>();
 		fishbone0 = new ArrayList<Rectangle>();
 		penguin = new Rectangle(WIDTH / 2, HEIGHT - 175, penguinwidth, penguinheight);
-		/* 신필 펭귄사이즈 -30 ㄱㄱ */
 		timer2.start();
 		timerfish0.start();
 		timerheart.start();
 		timerfishbone.start();
-		// 배경음악 재생
-//		loadMusic("audio/snow_bgm.wav");
-//		clipBackGroundMusic.start();
-//		clipBackGroundMusic.loop(clipBackGroundMusic.LOOP_CONTINUOUSLY);
+
+		loadMusic("audio/bgm.wav");
+		clipBackGroundMusic.start();
+		clipBackGroundMusic.loop(clipBackGroundMusic.LOOP_CONTINUOUSLY);
 	}
 
 	public class Mypanel extends JPanel {
@@ -110,16 +121,18 @@ public class Fish implements ActionListener, KeyListener {
 	}
 
 	public void repaint(Graphics g) {
-		ImageIcon i1 = new ImageIcon("image/back.jpg");
+		
 		Image image1 = i1.getImage();
-		g.drawImage(image1, 0, 0, WIDTH, HEIGHT, null);
-//		g.setColor(Color.white);
-//		g.fillRect(0, HEIGHT - 80, WIDTH, 120);
-		ImageIcon i2;
-		if(cnt%6 ==0 || cnt%6 ==1) {i2 = new ImageIcon("image/penguin1.png");}
-		else if(cnt%6 ==2 || cnt%6 ==3) {i2 = new ImageIcon("image/penguin2.png");}
-		else {i2 = new ImageIcon("image/penguin3.png");}
-		Image image2 = i2.getImage();
+		//g.drawImage(image1, 0, 0, WIDTH, HEIGHT, null);
+		g.drawImage(image1, 0, 0, WIDTH, HEIGHT, 0, 0+sy, 113, 500+sy, null);
+		sy++;
+		if(sy==300) sy=0;
+		
+		Image image2;
+		if(cnt%6 ==0 || cnt%6 ==1) {image2 = penguin1.getImage();}
+		else if(cnt%6 ==2 || cnt%6 ==3) {image2 = penguin2.getImage();}
+		else {image2 = penguin3.getImage();}
+		
 		cnt++;
 		if (xMotion > 0) {
 			g.drawImage(image2, penguin.x, penguin.y, penguin.x + penguinwidth, penguin.y + penguinheight, 0, 0, 2403, 2365, null);
@@ -133,23 +146,23 @@ public class Fish implements ActionListener, KeyListener {
 		}
 
 		for (int i = 0; i < life; i++) {
-			ImageIcon i3 = new ImageIcon("image/heart1.png");							//현재 라이프 이미지삽입
-			Image image3 = i3.getImage();
+			
+			Image image3 = iheart1.getImage();
 			g.drawImage(image3, 30 + i * 70, 30, 90 + i * 70, 90, 0, 0, 800, 800, null);
 		}
 		for (Rectangle fish : fish0) {
-			ImageIcon i0 = new ImageIcon("image/fish.png");
-			Image image0 = i0.getImage();
+			
+			Image image0 = ifish.getImage();
 			g.drawImage(image0, fish.x, fish.y, fish.x + 40, fish.y + 40, 0, 0, 400, 400, null);
 		}
 		for (Rectangle heart : heart0) {
-			ImageIcon i0 = new ImageIcon("image/heart0.png");
-			Image image0 = i0.getImage();
+			
+			Image image0 = iheart0.getImage();
 			g.drawImage(image0, heart.x, heart.y, heart.x + 40, heart.y + 40, 0, 0, 400, 400, null);
 		}
 		for (Rectangle fishbone : fishbone0) {
-			ImageIcon i0 = new ImageIcon("image/fishbone.png");
-			Image image0 = i0.getImage();
+			
+			Image image0 = ifishbone.getImage();
 			g.drawImage(image0, fishbone.x, fishbone.y, fishbone.x + 40, fishbone.y + 40, 0, 0, 400, 400, null);
 		}
 		g.setColor(Color.black);
@@ -167,17 +180,31 @@ public class Fish implements ActionListener, KeyListener {
 			timerfish0.stop();
 			timerheart.stop();
 			timerfishbone.stop();
+			
+		}
+		if (gameOver && !gameOverSound) {
+			loadAudio("audio/fail.wav");
+			clip.start();
+			gameOverSound = true;
 		}
 
 	}
 
 	public void actionPerformed(ActionEvent arg0) {
-		if(score==0) speed = 1;
-		if(score==10) speed=2;
-		if(score==20) speed=3;
-		if(score==30) speed=4;
-		if(score==40) speed=5;
-		if(score==50) speed=6;
+		
+		if(score==100) {speed=2;
+		timerfishbone = new Timer(1400, new ActionListener() { // 생선뼈 객체 생성
+			public void actionPerformed(ActionEvent arg0) {
+				int dropfishbone = rand.nextInt(WIDTH - 80);
+				fishbone0.add(new Rectangle(dropfishbone + 40, 0, 40, 40));
+			}
+		});
+		timerfishbone.start();
+		}
+		if(score==200) speed=4;
+		if(score==300) speed=6;
+		if(score==400) speed=8;
+		if(score==500) speed=12;
 		if (started) {
 			for (int i = 0; i < fish0.size(); i++) {
 				Rectangle fish = fish0.get(i);
@@ -187,13 +214,13 @@ public class Fish implements ActionListener, KeyListener {
 		if (started) {
 			for (int i = 0; i < heart0.size(); i++) {
 				Rectangle heart = heart0.get(i);
-				heart.y += speed + 1;
+				heart.y += speed;
 			}
 		}
 		if (started) {
 			for (int i = 0; i < fishbone0.size(); i++) {
 				Rectangle fishbone = fishbone0.get(i);
-				fishbone.y += speed + 1;
+				fishbone.y += speed+speed/2;
 			}
 		}
 		penguin.x += xMotion;
@@ -224,7 +251,7 @@ public class Fish implements ActionListener, KeyListener {
 		for (Iterator<Rectangle> it = fish0.iterator(); it.hasNext();) {
 			Rectangle value = it.next();
 			if (value.intersects(penguin)) {
-				score++;
+				score=score+10;
 				it.remove();
 				loadAudio("audio/fish.wav");
 				clip.start();
@@ -246,7 +273,7 @@ public class Fish implements ActionListener, KeyListener {
 			if (value.intersects(penguin)) {
 				life--;
 				it.remove();
-				loadAudio("audio/fishbone.mp3");
+				loadAudio("audio/fishbone.wav");
 				clip.start();
 			}
 			if (life <= 0) {
